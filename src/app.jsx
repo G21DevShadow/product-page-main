ae1a92cc9c086c3b5c995ee6cd5c788f9286d8d9
Shadow <xusernamex20@gmail.com>
2025-01-14 15:10:06 -0500

    comit inicial

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Hola mundo papu</div>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./app";
+import "./styles/main.css";
+
+createRoot(document.getElementById("rot")).render(<App />);
